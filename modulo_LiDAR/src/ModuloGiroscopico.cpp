e52c5c488f43d7c17f2653d1aeca68ffec1eb317
Arthur <arthurhenrrique1705@gmail.com>
2026-03-02 09:18:39 -0300

    Refatorando para classe ROS utilizavel

--- modulo_LiDAR/src/ModuloGiroscopico.cpp
+++ modulo_LiDAR/src/ModuloGiroscopico.cpp
@@ -1,7 +1,11 @@
 #include <mpu6050.h>
-#include <Wire.h>
 #include <cmath>
 #include <vector>
+#include <chrono>
+#include <memory>
+#include <string>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 const int TAMANHO_MAPA_VERTICAL = 26;
 const int TAMANHO_MAPA_HORIZONTAL = 26;
@@ -21,21 +25,44 @@ void calcularPontos();
 
 bool tabuleiro[TAMANHO_MAPA_VERTICAL][TAMANHO_MAPA_HORIZONTAL] = {};
 
-void setup(void) {
+using namespace std::chrono_literals;
 
-  configurarAcelerometro();
-  configurarGyroscopio();
-  configurarTermometro();
-  configurarSerial();
-  configurarMPU();
-  
-  Serial.println("");
-  delay(100);
-}
+ 
+class ModuloGiroscopico : public rclcpp::Node
+{
+public:
+  ModuloGiroscopico()
+  : Node("modulo_giroscopico"), count_(0)
+  {
+    configurarAcelerometro();
+    configurarGyroscopio();
+    configurarTermometro();
+    configurarSerial();
+    configurarMPU();
 
-void loop() {
-  calcularPontos();
-  delay(1000);
+    publisher_ = this->create_publisher<std_msgs::msg::String>("Radar", 10);
+    auto timer_callback =
+      [this]() -> void {
+        auto message = std_msgs::msg::String();
+        message.data = calcularPontos() + std::to_string(this->count_++);
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        this->publisher_->publish(message);
+      };
+    timer_ = this->create_wall_timer(500ms, timer_callback);
+  }
+
+private:
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
 }
 
 float calcularDistanciaY() {
