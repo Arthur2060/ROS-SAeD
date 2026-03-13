21da15ac6f4cc4eef00c863bf2a9643c2cfb5313
Arthur <arthurhenrrique1705@gmail.com>
2026-03-13 11:25:39 -0300

    Preparando subiscriber para integra-lo a rede ROS

--- src/modulo_radar/src/BatalhaNava.cpp
+++ src/modulo_radar/src/BatalhaNava.cpp
@@ -1,6 +1,9 @@
-#include "rclcpp/rclcpp.hpp" // Importa biblioteca ROS 2
-#include <cmath>             // Importa biblioteca para cálculos complexos em C++
-#include <stdio.h>           // Importa bibliotca para uso de printf
+#include <memory>
+#include "rclcpp/rclcpp.hpp"		// Importa biblioteca ROS 2
+#include "std_msgs/msg/string.hpp" 
+#include <cmath>             		// Importa biblioteca para cálculos complexos em C++
+#include <stdio.h>           		// Importa bibliotca para uso de printf
+#include <sstream>
 
 // Variaveis
 
@@ -17,21 +20,62 @@ double calcularX();     // Calcula o afastamento do alvo no eixo X
 double calcularY();     // Calcula o afastamento do alvo no eixo Y
 float anguloEmRadi();   // Calcula o equivalente em radianos do angulo em graus
 
+// Nota:
 // Depois eu faço a informação vir em radianos para poupar trabalho
+//
 
-int main() {
-	// Exibindo dados calculados
+// Criacao da entidade para increver no topico radar
+class BatalhaNavalSubscriber : public rclcpp::Node
+{
+public:
+  BatalhaNavalSubscriber() 
 
-    printf("%d graus = %.2f radianos\n", angulo, anguloEmRadi);
-	printf("A medida %.2f no angulo %d, indica um obstaculo a %.2f metros a %s e %.2f metros %s\n",
+  // Cria um construtor fazendo o código 
+  // seguinte ser iniciado sempre que uma
+  // mensagem for publicada no topico
+  : Node("batalha_naval_subscriber")
+  {
+	// Funcao para ouvir a mensagem e armazenar as infos nas variaveis
+    auto topic_callback =
+      [this](std_msgs::msg::String::UniquePtr msg) -> void {
+        RCLCPP_INFO(this->get_logger(), "Eu ouvi: '%s'", msg->data.c_str());
+		std::stringstream infos(msg->data); // Armazena a mensagem em uma String
+
+		std::string anguloRecebido;					
+		std::string distanciaRecebido;				
+
+		getline(infos, anguloRecebido, '\n');		// Coleta os dados linha a linha
+		getline(infos, distanciaRecebido, '\n');	// Coleta os dados linha a linha
+
+		angulo = std::stoi(anguloRecebido);					// Transfere as informações para as respectivas variaveis
+		distancia = std::stod(distanciaRecebido);				// Transfere as informações para as respectivas variaveis
+
+		double x = calcularX();
+		double y = calcularY();
+
+		// Exibição de resultados
+    	RCLCPP_INFO(this->get_logger(), "%d graus = %.2f radianos\n", angulo, anguloEmRadi());
+		RCLCPP_INFO(this->get_logger(), "A medida %.2f no angulo %d, indica um obstaculo a %.2f metros a %s e %.2f metros %s\n",
 	       distancia,
 	       angulo,
-	       calcularX(),
-	       calcularY(),
-	       (calcularX() > 0.0) ? "direita" : "esquerda", // Caso o valor de X seja negativo isso indica que o alvo esta a esquerda
-	       (calcularY() > 0.0) ? "a frente" : "atras"    // Caso o valor de Y seja negativo isso indica que o alvo esta a atras
-	      );
+	       x,
+	       y,
+	       (x > 0.0) ? "direita" : "esquerda", // Caso o valor de X seja negativo isso indica que o alvo esta a esquerda
+	       (y > 0.0) ? "a frente" : "atras"    // Caso o valor de Y seja negativo isso indica que o alvo esta a atras
+	    );
+      };
+    subscription_ =
+      this->create_subscription<std_msgs::msg::String>("radar", 10, topic_callback);
+  }
 
+private:
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[]) {
+	rclcpp::init(argc, argv);
+  	rclcpp::spin(std::make_shared<BatalhaNavalSubscriber>());
+  	rclcpp::shutdown();
 	return 0;
 }
 
@@ -47,16 +91,12 @@ double calcularX() {
     float seno = sin(anguloEmRadi());
     float distanciaX = seno * distancia;
     
-    printf("Seno(%d) = %.2f\n", angulo, seno);
-    printf("%.2f X %.2f = %.2f\n\n", seno, distancia, distanciaX);
 	return distanciaX;
 }
 
 double calcularY() {
     float coseno = cos(anguloEmRadi());
     float distanciaY = coseno * distancia;
-    
-    printf("Coseno(%d) = %.2f\n", angulo, coseno);
-    printf("%.2f X %.2f = %.2f\n\n", coseno, distancia, distanciaY);
+
 	return distanciaY;
 }
