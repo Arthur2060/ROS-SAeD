08bf7f63c62aec7cec835e69a6224e3e8afc650d
Arthur <arthurhenrrique1705@gmail.com>
2026-03-28 19:15:10 -0300

    Adicionando dockerfile construindo gitignore e reformulando BatalhaNaval para simples teste de leiitura de topico.

---
+++ src/package_radar/src/BatalhaNaval.cpp
@@ -0,0 +1,33 @@
+#include <memory>
+#include <cmath>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using std::placeholders::_1;
+
+class BatalhaNaval : public rclcpp::Node
+{
+public:
+  BatalhaNaval()
+  : Node("batalha_naval")
+  {
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&BatalhaNaval::topic_callback, this, _1));
+  }
+
+private:
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg->data);
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<BatalhaNaval>());
+  rclcpp::shutdown();
+  return 0;
+}
